8b5c4d9e9a5be58bca7b050cb773d8506fd9d80b
kawaemon <34652535+kawaemon@users.noreply.github.com>
2020-06-13 20:03:10 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/approvers/dockerps-web
+
+go 1.14
+
+require (
+	github.com/Microsoft/go-winio v0.4.14 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v1.13.1
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/gin-gonic/gin v1.6.3 // indirect
+	github.com/go-playground/validator/v10 v10.3.0 // indirect
+	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
+	golang.org/x/sys v0.0.0-20200610111108-226ff32320da // indirect
+	google.golang.org/protobuf v1.24.0 // indirect
+	gopkg.in/yaml.v2 v2.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+func main() {
+	docker, err := client.NewEnvClient()
+	if err != nil {
+		fmt.Println("Failed at connecting to docker")
+		fmt.Println(err)
+		return
+	}
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.LoadHTMLFiles("./template.html")
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "template.html", gin.H{
+			"containers": getContainers(docker),
+		})
+	})
+
+	fmt.Println("Start Serving...")
+	err = router.Run()
+	if err != nil {
+		fmt.Println("Failed at starting Web server")
+		fmt.Println(err)
+		return
+	}
+}
+
+func getContainers(docker *client.Client) (result []map[string]string) {
+	containers, err := docker.ContainerList(context.Background(), types.ContainerListOptions{});
+	if err != nil {
+		fmt.Println("Failed at getting container list")
+	}
+
+	result = make([]map[string]string, 0)
+
+	for _, container := range containers {
+		local := map[string]string {
+			"Image": container.Image,
+			"ID": container.ID[0:12],
+			"Command": container.Command,
+			"Created": formatUnixDate(container.Created),
+			"Status": container.Status,
+			"Ports": formatPortArray(container.Ports),
+			"Names": formatStringArray(container.Names),
+		}
+
+		result = append(result, local)
+	}
+	return
+}
+
+func formatUnixDate(unix int64) string {
+	durationFromNow := time.Now().Sub(time.Unix(unix, 0)).Truncate(time.Second)
+	return fmt.Sprintf("%s ago", durationFromNow.String())
+}
+
+func formatPortArray(array []types.Port) (result string) {
+	for _, v := range array {
+		result += fmt.Sprintf("%s:%d->%d/%s, ", v.IP, v.PrivatePort, v.PublicPort, v.Type)
+	}
+	return
+}
+
+func formatStringArray(array []string) (result string) {
+	for _, v := range array {
+		result += fmt.Sprintf("%s, ", v)
+	}
+	return
+}
